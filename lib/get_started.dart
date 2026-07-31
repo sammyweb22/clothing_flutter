a8da2a3a27bb60a719638808d3076c757212bbbf
sammyweb22 <91780397+sammyweb22@users.noreply.github.com>
2026-07-31 14:01:22 +0100

    Add get started screen

---
+++ lib/get_started.dart
@@ -0,0 +1,107 @@
+import 'package:clothing_flutter/home_screen.dart';
+import 'package:clothing_flutter/main.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GetStartedScreen extends StatelessWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // extendBody: true,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFAF52DE).withOpacity(.1),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                  ),
+                ),
+                child: Image.asset(
+                  "assets/images/kingdom-payment.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              height: height * .35,
+              color: Color(0xFFAF52DE).withOpacity(.1),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const Gap(40),
+                    Image.asset("assets/images/expenio.png"),
+                    const Gap(40),
+                    Text(
+                      'Going cashless has never been this easier with the world’s most leading expense manager.',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.inter(
+                        fontSize: 17,
+                        color: Color(0xFFAEAEB2),
+                      ),
+                    ),
+                    Spacer(),
+                    // TextButton(onPressed: () => {}, child: Text('Get Started')),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: const LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFF12B0F8), // Top color
+                            Color(0xFF007AFF), // Bottom color
+                          ],
+                        ),
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeScreen()));
+                          Navigator.pushReplacementNamed(context, '/home');
+                        },
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 30,
+                            vertical: 15,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          foregroundColor: Colors.white, // Text color
+                        ),
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Gap(30),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
